74fbfe8905e69195dedfb39a0c133aea3592de44
Amares03 <amaressabit0314@gmail.com>
2020-12-27 12:44:45 +0300

    first commit

---
+++ convert_binary_t_decimal.py
@@ -0,0 +1,13 @@
+def convert(str(var)):
+    s=str(var)
+    leng=len(str(var))-1
+    ln=leng
+    lng=0
+    ans=var[ln]*2**lng
+    while lng == leng:
+        ans
+        lng=lng+1
+        ln=ln-1
+        print(ans)
+
+convert(1011)
